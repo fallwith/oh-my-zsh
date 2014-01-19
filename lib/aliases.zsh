17b19291450134e134045a2ddc2daa99b9fa2f4e
fallwith <fallwith@gmail.com>
2014-01-18 20:39:10 -0800

    change the 'll' alias

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -28,7 +28,8 @@ fi
 # List direcory contents
 alias lsa='ls -lah'
 alias l='ls -la'
-alias ll='ls -l'
+#alias ll='ls -l'
+alias ll='ls -lApG --color=always'
 alias la='ls -lA'
 alias sl=ls # often screw this up
 
